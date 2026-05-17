d02a079a7f8a786efd93d2cdacfc0ce91e54aa8c
LDinos <ldinos99@gmail.com>
2026-05-17 20:56:07 +0300

    Crete resource directory automatically

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Client, GatewayIntentBits } from 'discord.js';
 import { registerCommands, deleteAllCommands, interactionCommands } from './register_commands.js';
 import { clearResourceFolder } from './utils.js';
 import chalk from 'chalk';
+import fs from 'fs';
 
 export const client = new Client({
     intents: [
@@ -16,6 +17,10 @@ export const client = new Client({
 
 client.once('clientReady', async () => {
     console.log(chalk.green(`Logged in as ${client.user.tag}`));
+    if (!fs.existsSync('resource')) {
+        fs.mkdirSync('resource');
+        console.log('Created resource directory for first time');
+    }
     clearResourceFolder();
     await deleteAllCommands();
     await registerCommands();
